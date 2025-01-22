e56cffceee62255a52e20b9bc298b53261139964
Kenneth Mburu <123557828+Kenneth12222@users.noreply.github.com>
2025-01-22 22:28:28 +0300

    Update main.rs
    get method and post methods for api request, Appstete for storing data user post

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,20 @@ struct Post {
 struct AppState {
     posts: Mutex<Vec<Post>>,
 }
+
+#[actix_web::get("/posts")]
+async fn get_posts(data: web::Data<AppState>) -> impl Responder {
+    let posts = data.posts.lock().unwrap();
+    HttpResponse::Ok().json(&*posts)
+}
+
+#[actix_web::post("/posts")]
+async fn create_post(data: web::Data<AppState>, new_post: web::Json<Post>) -> impl Responder {
+    let mut posts = data.posts.lock().unwrap();
+    posts.push(Post {
+        id: posts.len() + 1,
+        title: new_post.title.clone(),
+        content: new_post.content.clone(),
+    });
+    HttpResponse::Ok().json(&*posts)
+}
